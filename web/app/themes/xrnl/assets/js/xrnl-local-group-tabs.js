a74b457e746919768cec5af7533344801c287d3b
franei <frank@yora.net>
2021-02-20 18:21:41 +0100

    Check if a specific tab was requested

--- web/app/themes/xrnl/assets/js/xrnl-local-group-tabs.js
+++ web/app/themes/xrnl/assets/js/xrnl-local-group-tabs.js
@@ -23,6 +23,13 @@ jQuery(document).ready(function($){
     $('#lg-navigation .nav .nav-item').remove();
   };
 
+  // See if a specific tab was requested in the URL
+  var requestedTab = '';
+  let URLParam = window.location.search.substring(1);
+  if (sections.includes(URLParam)) {
+    requestedTab = URLParam;
+  }
+
   // If the About Us section exists, make it active on page load (instead of Contact).
   if ($('#about-nav').length) {
     $('#about-nav').tab('show');
